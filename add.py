d97493a7934eb1f3673ded4ccedd3e7470027bb7
Hey-Ishwar <ishwarsharansingh4@gmail.com>
2024-09-27 16:26:48 +0530

    testing conflict

--- add.py
+++ add.py
@@ -1,3 +1,4 @@
 a = int(input("Enter a number : ",))
 b=int(input("Enter a number : ",))
-print("Sum of them are:", (a+b))
+print("Sum of them are as follows:", (a+b))
+print("Testing merge conflicts")
